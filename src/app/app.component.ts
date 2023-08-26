9a415634815be4db3e0498f7404408a7070c1f6c
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-08-26 18:35:33 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { products as data } from './data/products';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'crash-course';
+  title = 'Angular crash course';
+  products: IProduct[] = data;
 }
